6b3b23fc8af20a0e2b619ffc174360361493196f
AcridStingray3 <acridstingray3@gmail.com>
2021-10-03 14:49:08 +0200

    Better logger format
    Not perfect but I cannot be bothered

--- TexitArchenemy/Services/Logger/ArchenemyLogger.cs
+++ TexitArchenemy/Services/Logger/ArchenemyLogger.cs
@@ -9,8 +9,8 @@ namespace TexitArchenemy.Services.Logger
     {
         public static async Task Log(string message, string from, bool addDatetime = true)
         {
-            Task fileWrite = File.AppendAllTextAsync("log.txt", $"{(addDatetime?DateTime.Now + " " :string.Empty)}{from} {message}");
-            Console.WriteLine(message);
+            Task fileWrite = File.AppendAllTextAsync("log.txt", $"{(addDatetime?" "+ DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss")  + " " :string.Empty)}{from} {message}");
+            Console.WriteLine($"{(addDatetime?" " +DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss") + " " :string.Empty)}{from} {message}");
             await fileWrite;
         }
 
