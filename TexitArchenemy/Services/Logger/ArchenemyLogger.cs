708f2963b9dd4bfe7760b84520895bc74b1bdc6f
AcridStingray3 <acridstingray3@gmail.com>
2021-10-24 11:41:44 +0200

    Make sure only one Task gets the file at a time

--- TexitArchenemy/Services/Logger/ArchenemyLogger.cs
+++ TexitArchenemy/Services/Logger/ArchenemyLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Discord;
 
@@ -7,11 +8,14 @@ namespace TexitArchenemy.Services.Logger
 {
     public static class ArchenemyLogger
     {
+        private static readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
         public static async Task Log(string message, string from, bool addDatetime = true)
         {
+            await _semaphoreSlim.WaitAsync();
             Task fileWrite = File.AppendAllTextAsync(@"Logger\log.txt", $"{(addDatetime?" "+ DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss")  + " " :string.Empty)}{from} {message} {Environment.NewLine}");
             Console.WriteLine($"{(addDatetime?" " +DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss") + " " :string.Empty)}{from}\t{message}");
             await fileWrite;
+            _semaphoreSlim.Release();
         }
 
         public static async Task Log(LogMessage arg)
