f01356693f5388459f4a70d2c066139b64c90f1d
BasedJellyfish11 <acridstingray3@gmail.com>
2022-07-13 03:06:13 +0200

    Venom command

---
+++ TexitArchenemy/Services/Discord/Commands/Puma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using JetBrains.Annotations;
+using TexitArchenemy.Services.Logger;
+
+namespace TexitArchenemy.Services.Discord.Commands;
+
+public class PumaModule: ModuleBase<SocketCommandContext>
+{
+    private static readonly Random random = new();
+    
+    [Command("Puma")]
+    [Summary("RRRRRRRRRAAAAAAAAAAAGH")]
+    [UsedImplicitly]
+    public async Task Coinflip()
+    {
+        List<string> files = Directory.EnumerateFiles("Venom/").ToList();
+        
+        await Context.Channel.SendFileAsync(files[random.Next(0, files.Count)], "skeleton.png");
+            
+    }
+}
